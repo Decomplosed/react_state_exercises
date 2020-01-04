05c9387dc28492ba6e538603647d0a90fb780b3c
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-04 23:55:14 +0100

    changed the order of the elements

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -40,8 +40,8 @@ class CoinContainer extends Component {
     return (
       <div className='CoinContainer'>
         <h2>Let's flip a coin!</h2>
-        <button onClick={this.handleClick}>Flip Me!</button>
         {this.state.currCoin && <Coin info={this.state.currCoin} />}
+        <button onClick={this.handleClick}>Flip Me!</button>
         <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
       </div>
     )
