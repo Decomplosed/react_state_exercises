f5eabe291880a6507b2504b4d989b3848775f266
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-04 23:14:10 +0100

    created a paragraph which will show the amount of flips, tails and heads

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -21,6 +21,7 @@ class CoinContainer extends Component {
     return (
       <div className='CoinContainer'>
         <h2>Let's flip a coin!</h2>
+        <p>Out of {this.state.nFilips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails.</p>
       </div>
     )
   }
