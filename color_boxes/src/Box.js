99ad3e63f2c212d6706d5a2389a4f1ceaf61c924
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-05 12:46:48 +0100

    moved colors array to the BoxContainer component

--- color_boxes/src/Box.js
+++ color_boxes/src/Box.js
@@ -3,17 +3,9 @@ import { choice } from './helpers'
 import './Box.css'
 
 class Box extends Component {
-  static defaultProps = {
-    allColors: [
-      'purple',
-      'magenta',
-      'violet',
-      'pink'
-    ]
-  }
   constructor(props) {
     super(props)
-    this.state = { color: choice(this.props.allColors) }
+    this.state = { color: choice(this.props.colors) }
     this.handleClick = this.handleClick.bind(this)
   }
 
@@ -21,7 +13,7 @@ class Box extends Component {
     let newColor
 
     do {
-      newColor = choice(this.props.allColors)
+      newColor = choice(this.props.colors)
     } while (newColor === this.state.color)
 
     this.setState({ color: newColor })
