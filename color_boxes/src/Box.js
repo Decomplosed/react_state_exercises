9394e27aff3f00a1d55dcb551a845dba6aedfc03
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-05 12:17:25 +0100

    started creating a Box component

---
+++ color_boxes/src/Box.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Box extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { color: 'purple' }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Box
