9576c7d802bf9af38d284f785d6e562d5e0649b3
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-05 12:38:32 +0100

    import Box component

--- color_boxes/src/BoxContainer.js
+++ color_boxes/src/BoxContainer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Box from './Box'
 
 class BoxContainer extends Component {
   static defaultProps = {
     numBoxes: 18
   }
   render() {
+    return (
 
+    )
   }
 }
 
