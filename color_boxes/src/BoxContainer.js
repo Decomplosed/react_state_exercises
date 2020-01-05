7b6a9b737f4508c36caa7aa5c0a05898fa89afab
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-05 12:36:47 +0100

    defined defaultProps

--- color_boxes/src/BoxContainer.js
+++ color_boxes/src/BoxContainer.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
 class BoxContainer extends Component {
+  static defaultProps = {
+    numBoxes: 18
+  }
+  render() {
 
+  }
 }
 
 export default BoxContainer
